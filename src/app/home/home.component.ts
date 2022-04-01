2666fe8bc41679f6c434ff13b15bf9617ea19e13
cmgann <casey.m.gann@gmail.com>
2022-04-01 17:28:28 -0500

    fixed issue2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public innerWidth: any;
+  cols!: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    console.log(this.innerWidth)
+    this.changeMatGrid()
+  }
+
+  @HostListener('window:resize', ['$vent'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth
+    this.changeMatGrid()
+  }
+
+  changeMatGrid(){
+    if(this.innerWidth < 755) {
+      this.cols = 1
+    }
+    else {
+      this.cols = 2
+    }
   }
 
 }
